e04b63422f06c3eaa0b392e72febbf088c148376
FuzzyUmbrella2 <m.trietsch.game@gmail.com>
2025-09-30 11:32:14 +0200

    Update Stitcher.cpp

--- MatrixMult/Stitcher.cpp
+++ MatrixMult/Stitcher.cpp
@@ -2,7 +2,15 @@
 #include <cstdint>
 #include <iostream>
 
-void matrixStitcher()
+void matrixStitcher(matrix_out_t input_matrix[MATRIX_SIZE][MATRIX_SIZE],
+                    matrix_out_t output_matrix[MATRIX_SIZE][MATRIX_SIZE],
+                    uint16_t size)
 {
-    
+    for(uint16_t row=0; row<MATRIX_SIZE; ++row)
+    {
+        for(uint16_t col=0; col<MATRIX_SIZE; ++col)
+        {
+            output_matrix[row][col]+=input_matrix[row][col];
+        }
+    }
 }
