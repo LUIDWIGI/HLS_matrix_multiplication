29fc690fbb52d507349d85f2d685623673ce130e
FuzzyUmbrella2 <m.trietsch.game@gmail.com>
2025-10-18 12:40:24 +0200

    maxi update

--- MatrixMult/MatrixMult.cpp
+++ MatrixMult/MatrixMult.cpp
@@ -7,25 +7,17 @@ typedef int16_t matrix_16_t;
 typedef int32_t matrix_out_t;
 typedef int32_t matrix_32_t;
 
-void matrixSlicer(matrix_in_t* matrix_in_1,
-                 matrix_in_t* matrix_in_2,
-                 uint32_t size,
-                 matrix_out_t matrix_out_1[MATRIX_SIZE][MATRIX_SIZE],
-                 matrix_out_t matrix_out_2[MATRIX_SIZE][MATRIX_SIZE]);
-
 void MatrixMult(matrix_in_t* matrix_in_1,
                 matrix_in_t* matrix_in_2,
                 uint16_t size,
                 matrix_out_t* matrix_out)
 {
-#pragma HLS INTERFACE mode=m_axi port=matrix_out bundle=data
-#pragma HLS INTERFACE mode=m_axi port=matrix_in_2 bundle=data1
-#pragma HLS INTERFACE mode=m_axi port=matrix_in_1 bundle=data
+#pragma HLS INTERFACE mode=m_axi port=matrix_in_1 bundle=BUS_A channel=0 depth=(MAX_MATRIX_SIZE*MAX_MATRIX_SIZE)
+#pragma HLS INTERFACE mode=m_axi port=matrix_in_2 bundle=BUS_A channel=1 depth=(MAX_MATRIX_SIZE*MAX_MATRIX_SIZE)
+#pragma HLS INTERFACE mode=m_axi port=matrix_out bundle=BUS_A channel=0 depth=(MAX_MATRIX_SIZE*MAX_MATRIX_SIZE)
 
     matrix_in_t matrix1[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE];
     matrix_in_t matrix2[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE];
-#pragma HLS BIND_STORAGE variable=matrix2 type=ram_2p impl=bram
-#pragma HLS BIND_STORAGE variable=matrix1 type=ram_2p impl=bram
 #pragma HLS ARRAY_PARTITION variable=matrix2 dim=0 type=complete
 #pragma HLS ARRAY_PARTITION variable=matrix1 dim=0 type=complete
 
@@ -35,13 +27,14 @@ void MatrixMult(matrix_in_t* matrix_in_1,
         {
             matrix1[x][y] = matrix_in_1[x*size+y];
             matrix2[x][y] = matrix_in_2[x*size+y];
+            std::cout << matrix2[x][y] << " ";
         }
+        std::cout << std::endl;
     }
  
     for(uint16_t j=0; j<(size/MATRIX_SIZE)*(size/MATRIX_SIZE); ++j)
     {
         matrix_out_t Matrix_4x4_final[MATRIX_SIZE][MATRIX_SIZE] = {};
-#pragma HLS BIND_STORAGE variable=Matrix_4x4_final type=ram_2p
         matrix_32_t matrix_4x4_out[MATRIX_SIZE][MATRIX_SIZE];
         for(uint16_t i=0; i<(size/MATRIX_SIZE); ++i)
         {
