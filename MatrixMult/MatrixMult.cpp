60c438d8e9c2308f17b85690a3e3ffa5cc30e9ba
LUIDWIGI <empireprojectaltijd@gmail.com>
2025-10-22 15:11:14 +0200

    Readded lost interface for size and AP_CTRL

--- MatrixMult/MatrixMult.cpp
+++ MatrixMult/MatrixMult.cpp
@@ -12,6 +12,8 @@ void MatrixMult(matrix_in_t* matrix_in_1,
                 uint16_t size,
                 matrix_out_t* matrix_out)
 {
+#pragma HLS INTERFACE s_axilite port=size bundle=ctrl
+#pragma HLS INTERFACE s_axilite port=return bundle=ctrl
 #pragma HLS CACHE port=matrix_in_2 depth=MAX_MATRIX_SIZE lines=MATRIX_SIZE*4
 #pragma HLS CACHE port=matrix_in_1 depth=MAX_MATRIX_SIZE lines=MATRIX_SIZE*4
     assert(size%2==0);
@@ -24,7 +26,7 @@ void MatrixMult(matrix_in_t* matrix_in_1,
     #pragma HLS dataflow
     #pragma HLS LOOP_TRIPCOUNT max=(MAX_MATRIX_SIZE/MATRIX_SIZE * MAX_MATRIX_SIZE/MATRIX_SIZE)
         matrix_out_t Matrix_4x4_final[MATRIX_SIZE][MATRIX_SIZE] = {};
-        #pragma HLS ARRAY_PARTITION variable=Matrix_4x4_final dim=0 type=complete   
+        #pragma HLS ARRAY_PARTITION variable=Matrix_4x4_final dim=0 type=complete
         for(ap_uint<11> i=0; i<(size/MATRIX_SIZE); ++i)
         {
             #pragma HLS LOOP_TRIPCOUNT max=MAX_MATRIX_SIZE/MATRIX_SIZE
@@ -37,7 +39,7 @@ void MatrixMult(matrix_in_t* matrix_in_1,
             matrixSlicer(matrix_in_1, matrix_in_2, size, Matrix_4x4_1, Matrix_4x4_2);
             multiplier(Matrix_4x4_1, Matrix_4x4_2, matrix_4x4_out);
             matrixAdder(matrix_4x4_out, Matrix_4x4_final);
-        }        
+        }
         matrixStitcher(Matrix_4x4_final, size, matrix_out);
     }
 }
